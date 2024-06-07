5e728951912928fb87c0b7d473d528f2afadf06c
Gayathri1800 <gayathritedi18@gmail.com>
2024-06-07 13:39:38 +0000

    Update Example.java

--- Example.java
+++ Example.java
@@ -1,7 +1,7 @@
 /**
 * The HelloWorldApp class implements an application that simply prints "Hello World"
 */
-public class DevOpsExample {
+public class Example {
 public static void main(String[] args) {
 System.out.println("Welcome to DevOps.");
 }
